4eb9ff26355be1da671aeb1bc60790a6ddbe137b
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2024-07-03 02:20:28 +0530

    chore: add chinni changes 19259

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './assets/zenharuki-logo.png';
 import sparkle from './assets/sparkle-logo.svg';
 import perfios from './assets/perfios-logo.jpeg';
 import rtsn from './assets/rtsn-logo.jpeg';
@@ -67,10 +66,7 @@ export default function App() {
 
   return (
     <div>
-      <a href="/" className="absolute top-0 left-0 right-0 mx-auto mt-4 w-56 h-auto pt-8">
-        <img src={logo} alt="Logo" className="w-full h-auto" />
-      </a>
-      <div className="relative">
+      <div className="relative text-left">
         <div className="absolute right-4 top-1 transform -translate-y-1/2 w-8 h-auto">
           <img src={sparkle} alt="Sparkle Logo" className="w-8 h-auto mb-4" />
         </div>
@@ -78,7 +74,7 @@ export default function App() {
           <img src={sparkle} alt="Sparkle Logo" className="w-8 h-auto mt-4" />
         </div>
 
-        <main className="max-w-4xl mx-auto p-4 font-primaryRegular space-y-8 mt-16">
+        <main className="max-w-4xl mx-auto p-4 font-primaryRegular space-y-8 mt-8">
           <section className="pt-11 md:pt-8">
             <div className="grid-container mx-auto">
               <div className="grid grid-cols-1 md:grid-cols-16">
@@ -114,23 +110,23 @@ export default function App() {
               </div>
             </div>
             <hr />
-            <div className="experience-timeline relative flex flex-col items-center mt-8">
+            <div className="experience-timeline relative flex flex-col items-start mt-8">
               {years.map((year) => (
                 <div key={year} className="timeline-branch relative mb-8">
-                    <button
-                      className={`text-lg font-medium py-2 px-4 rounded-full ${
-                        currentYear === year ? 'bg-blue-500 text-white' : 'bg-gray-200 hover:bg-gray-300'
-                      }`}
-                      onClick={() => handleYearClick(year)}
-                    >
-                      {year}
-                    </button>
+                  <button
+                    className={`text-lg font-medium py-2 px-4 rounded-full ${
+                      currentYear === year ? 'bg-blue-500 text-white' : 'bg-gray-200 hover:bg-gray-300'
+                    }`}
+                    onClick={() => handleYearClick(year)}
+                  >
+                    {year}
+                  </button>
                   {currentYear === year && (
-                    <div className="timeline-content ml-12 mt-4 md:ml-0 md:mr-8">
+                    <div className="timeline-content ml-4 mt-4">
                       {experiences[year].map((job, index) => (
                         <div key={`${year}-${index}`} className="mb-6">
                           <div className="flex items-start">
-                          <img src={job.logo} alt="Company Logo" className="w-10 h-10 rounded-full mr-4" />
+                            <img src={job.logo} alt="Company Logo" className="w-10 h-10 rounded-full mr-4" />
                             <div>
                               <h3 className="text-lg md:text-xl font-bold mb-1">{job.title}</h3>
                               <p className="text-sm text-gray-600">{job.company}</p>
@@ -194,11 +190,10 @@ export default function App() {
             </div>
           </section>
           <section id="footer" className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-20 p-4 sm:p-20">
-            <div className="text-center sm:text-left mt-auto">
-              <p className="text-sm">Copyright © {new Date().getFullYear()}</p>
+            <div className="text-left mt-auto">
               <p className="text-sm">ZENHARUKI</p>
             </div>
-            <div className="flex justify-center sm:justify-end space-x-8 mt-auto">
+            <div className="flex justify-start space-x-8 mt-auto">
               <div className="opacity-100 hover:opacity-50 transition-opacity">
                 <a href="https://www.instagram.com/zenharuki/" target="_blank" rel="noopener noreferrer">Instagram</a>
               </div>
