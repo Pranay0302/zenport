1d3dec1a0a57585e08862d56068fcd6fc286f209
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2024-07-04 17:28:28 +0530

    chore: add cursor tracker in footer 1729

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import perfios from './assets/perfios-logo.jpeg';
 import rtsn from './assets/rtsn-logo.jpeg';
 import ril from './assets/ril-logo.jpeg';
@@ -8,6 +8,7 @@ export default function App() {
   const [isExpanded, setIsExpanded] = useState(false);
   const [currentYear, setCurrentYear] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
   const years = [2024, 2023, 2022, 2021];
   const experiences = {
@@ -61,6 +62,17 @@ export default function App() {
     ],
   };
 
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setCursorPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   const handleYearClick = (year) => {
     if (!isExpanded) {
       setIsExpanded(true);
@@ -97,11 +109,11 @@ export default function App() {
           </div>
         ))}
         <button className="mt-4 text-blue-500 bg-white hover:text-blue-800 hover:bg-gray-200 flex items-center justify-center px-4 py-2 border-2 border-black rounded-md transition-colors duration-300" onClick={handleModalClose}>
-        Close
-      </button>
+          Close
+        </button>
       </div>
     </div>
-  );  
+  );
 
   return (
     <div>
@@ -114,7 +126,7 @@ export default function App() {
               <div className="grid grid-cols-1 md:grid-cols-16">
                 <div className="md:col-span-10 lg:col-span-9 mb-14">
                   <div className="rich-text-hero">
-                    <p className="text-2xl md:text-2xl leading-relaxed tracking-wider">
+                    <p className="text-xl md:text-xl leading-relaxed tracking-wider">
                       I&apos;m <span className="text-blue-500">Pranay Kumar Andra</span>, an incoming graduate student in the MSCS program at the University of Colorado, Boulder. With practical industry exposure and a passion for pioneering software solutions. If you&apos;d like to contact me,&nbsp;
                       <a
                         className="text-blue-500 hover:text-blue-700"
@@ -136,45 +148,45 @@ export default function App() {
           </section>
 
           <section className="my-8 mx-auto max-w-4xl">
-          <div className="grid-container mx-auto">
-            <div className="grid grid-cols-1 md:grid-cols-12 pt-3 pb-3">
-              <div className="col-span-12">
-                <h2 className="text-xs uppercase font-medium mb-4">Experience Timeline</h2>
-                <p className="text-xl md:text-xl leading-relaxed">
-                  Throughout the years, I have been honored to work with small and large companies. Here are a few featured engagements.
-                </p>
+            <div className="grid-container mx-auto">
+              <div className="grid grid-cols-1 md:grid-cols-12 pt-3 pb-3">
+                <div className="col-span-12">
+                  <h2 className="text-xs uppercase font-medium mb-4">Experience Timeline</h2>
+                  <p className="text-xl md:text-xl leading-relaxed">
+                    Throughout the years, I have been honored to work with small and large companies. Here are a few featured engagements.
+                  </p>
+                </div>
+              </div>
+              <hr />
+              <div className="experience-timeline relative flex items-center justify-center mt-8">
+                <div className="flex flex-col">
+                  <button
+                    className="text-lg font-medium py-2 px-4 rounded-full bg-gray-200 hover:bg-gray-300 mb-4 transition-colors duration-300"
+                    onClick={() => {
+                      setIsExpanded(!isExpanded);
+                      setCurrentYear(null);
+                    }}
+                  >
+                    {isExpanded ? 'Collapse Years' : 'Expand Years'}
+                  </button>
+                  {isExpanded && (
+                    <div className="flex">
+                      {years.map((year) => (
+                        <button
+                          key={year}
+                          className={`text-lg font-medium py-2 px-4 rounded-full ${
+                            currentYear === year ? 'bg-blue-500 text-white' : 'bg-gray-200 hover:bg-gray-300'
+                          } transition-colors duration-300 mr-4 mb-4`}
+                          onClick={() => handleYearClick(year)}
+                        >
+                          {year}
+                        </button>
+                      ))}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-            <hr />
-            <div className="experience-timeline relative flex items-center justify-center mt-8">
-              <div className="flex flex-col">
-                <button
-                  className="text-lg font-medium py-2 px-4 rounded-full bg-gray-200 hover:bg-gray-300 mb-4 transition-colors duration-300"
-                  onClick={() => {
-                    setIsExpanded(!isExpanded);
-                    setCurrentYear(null);
-                  }}
-                >
-                  {isExpanded ? 'Collapse Years' : 'Expand Years'}
-                </button>
-                {isExpanded && (
-                  <div className="flex">
-                    {years.map((year) => (
-                      <button
-                        key={year}
-                        className={`text-lg font-medium py-2 px-4 rounded-full ${
-                          currentYear === year ? 'bg-blue-500 text-white' : 'bg-gray-200 hover:bg-gray-300'
-                        } transition-colors duration-300 mr-4 mb-4`}
-                        onClick={() => handleYearClick(year)}
-                      >
-                        {year}
-                      </button>
-                    ))}
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
           </section>
 
           <section className="my-8">
@@ -182,7 +194,7 @@ export default function App() {
               <div className="grid grid-cols-1 md:grid-cols-10">
                 <div className="md:col-span-9 lg:col-span-9 mb-8">
                   <div className="rich-text-hero">
-                    <p className="text-2xl md:text-2xl leading-relaxed">
+                    <p className="text-xl md:text-xl leading-relaxed">
                       Driven by a passion for shaping ideas, visual elements, and motion into compelling works, with a strong affinity for minimalism, modern architecture, and abstract art.
                     </p>
                   </div>
@@ -196,7 +208,7 @@ export default function App() {
               <div className="grid grid-cols-1 md:grid-cols-10">
                 <div className="md:col-span-9 lg:col-span-9 mb-8">
                   <div className="rich-text-hero">
-                    <p className="text-3xl md:text-3xl leading-relaxed">
+                    <p className="text-xl md:text-xl leading-relaxed">
                       Feel free to reach out and connect with me on social media or via email.{' '}
                       <a
                         className="text-blue-500 hover:text-blue-700"
@@ -222,9 +234,10 @@ export default function App() {
               </div>
             </div>
           </section>
-          <section id="footer" className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-20 p-4 sm:p-20">
+          <section id="footer" className="grid grid-cols-1 sm:grid-cols-2 gap-8 sm:gap-20 p-4 relative">
             <div className="text-left mt-auto">
-              <p className="text-sm">ZENHARUKI</p>
+              <p className="text-sm tracking-widest">ZENHARUKI</p>
+              <p className="text-sm">{cursorPosition.x} (X) , {cursorPosition.y} (Y)</p>
             </div>
             <div className="flex justify-start space-x-8 mt-auto">
               <div className="opacity-100 hover:opacity-50 transition-opacity">
