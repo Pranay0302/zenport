00bb015f03c441d07afbd9ae538997ae0c580f98
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2024-07-06 18:54:38 +0530

    chore: add ga 1853

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA from "react-ga4";
 
+
+ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
